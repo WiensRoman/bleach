5e4dd843214f83e83f67c69c6ae8b1b6e9ff985b
Roman Wiens <wiensroman95@gmail.com>
2024-08-05 05:00:58 +0300

    add thirdslide2 file

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <script setup>
 import FirstSlide from '../components/FirstSlide.vue'
 import SecondSlide from '../components/SecondSlide.vue'
-import ThirdSlide from '../components/ThirdSlide.vue'
+import ThirdSlide2 from '../components/ThirdSlide2.vue'
 import FourthSlide from '../components/FourthSlide.vue'
 import FifthSlide from '../components/FifthSlide.vue'
 import SixthSlide from '../components/SixthSlide.vue'
@@ -11,7 +11,7 @@ import SeventhSlide from '../components/SeventhSlide.vue'
   <Header></Header>
   <FirstSlide />
   <SecondSlide />
-  <ThirdSlide />
+  <ThirdSlide2 />
   <FourthSlide />
   <FifthSlide />
   <SixthSlide />
